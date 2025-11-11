c925c7f7193cd4e7272333934ccea00897fcfb1c
Melissa Salazar <95241712+melissaesalazarc@users.noreply.github.com>
2025-11-11 00:04:15 -0600

    responsive

--- my-portfolio/src/components/Navbar.tsx
+++ my-portfolio/src/components/Navbar.tsx
@@ -1,16 +1,25 @@
+import { useState } from "react";
 import "./Navbar.css";
 
 export default function Navbar() {
+  const [isOpen, setIsOpen] = useState(false);
+
   return (
     <nav className="nav">
       <div className="logo">Melissa's Portfolio</div>
 
-      <ul className="nav-links">
-        <li><a href="#">Home</a></li>
-        <li><a href="#about">About</a></li>
-        <li><a href="#projects">Projects</a></li>
-        <li><a href="#contact">Contact</a></li>
-      </ul>
+      <div className={`nav-links ${isOpen ? "open" : ""}`}>
+        <a href="#" onClick={() => setIsOpen(false)}>Home</a>
+        <a href="#about" onClick={() => setIsOpen(false)}>About</a>
+        <a href="#projects" onClick={() => setIsOpen(false)}>Projects</a>
+        <a href="#contact" onClick={() => setIsOpen(false)}>Contact</a>
+      </div>
+
+      <div className="hamburger" onClick={() => setIsOpen(!isOpen)}>
+        <span></span>
+        <span></span>
+        <span></span>
+      </div>
     </nav>
   );
 }
